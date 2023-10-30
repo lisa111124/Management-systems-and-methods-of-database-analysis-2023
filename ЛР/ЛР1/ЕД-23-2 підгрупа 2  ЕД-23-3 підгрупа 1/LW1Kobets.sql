4a2472d6e980aec9c3cc099ca41a8f65c5d28d13
VHKobets <kobets.vh@ef.dnu.edu.ua>
2023-10-30 22:26:06 +0200

    Create LW1Kobets.sql

---
+++ ЛР/ЛР1/ЕД-23-2 підгрупа 2  ЕД-23-3 підгрупа 1/LW1Kobets.sql
@@ -0,0 +1,19 @@
+SELECT *
+
+FROM [dbo].[CUSTOMERS];
+
+SELECT *
+
+FROM [dbo].[OFFICES];
+
+SELECT *
+
+FROM [dbo].[ORDERS];
+
+SELECT *
+
+FROM [dbo].[PRODUCTS];
+
+SELECT *
+
+FROM [dbo].[SALESREPS];
