ac601f8cb5ae35e20c1dc4337f38c2d94655d496
vaseletsvika <150890282+vaseletsvika@users.noreply.github.com>
2023-11-22 16:15:04 +0200

    Add files via upload

---
+++ ЛР/ЛР3/vaselets3.sql
@@ -0,0 +1,6 @@
+CREATE TABLE Extracurricular (
+        ActivityID INT PRIMARY KEY,
+        StudentID INT FOREIGN KEY REFERENCES PersonalInfo(StudentID),
+        ActivityName NVARCHAR(100),
+        JoinDate DATE
+    );
