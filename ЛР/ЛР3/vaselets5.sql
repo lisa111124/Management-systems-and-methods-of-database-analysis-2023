d02af02fc9527b26b34bfbc4a713b27c857ccc2a
BelozertsevVS <bvs.wl1980@gmail.com>
2023-11-23 21:13:25 +0200

    LW6
    LW6

--- ЛР/ЛР3/vaselets5.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE AcademicInfo (
-        RecordID INT PRIMARY KEY,
-        StudentID INT FOREIGN KEY REFERENCES PersonalInfo(StudentID),
-        Faculty NVARCHAR(150),
-        Curator NVARCHAR(100),
-        EnrollmentYear INT
-ALTER TABLE AcademicInfo
-ALTER COLUMN Faculty NVARCHAR(150);
-);
