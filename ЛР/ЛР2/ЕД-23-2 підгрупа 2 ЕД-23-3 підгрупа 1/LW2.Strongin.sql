a917c428d34fff5d4c9dcf8d5676200874d9b182
NStrongin <57713910+NStrongin@users.noreply.github.com>
2023-11-30 00:20:31 +0200

    Create LW2.Strongin.sql
    Added sql request for LW2

---
+++ ЛР/ЛР2/ЕД-23-2 підгрупа 2 ЕД-23-3 підгрупа 1/LW2.Strongin.sql
@@ -0,0 +1,58 @@
+USE [WebStor];
+
+
+GO
+
+CREATE SCHEMA STRONGIN; -- Ваше прізвище
+
+GO
+
+
+-- Create a simple table to demonstrate data types
+
+CREATE TABLE STRONGIN.DATATYPES (
+
+ID BIGINT, -- Unique identifier of an entity
+
+AGE TINYINT -- Number of years
+
+);
+
+INSERT INTO STRONGIN.DATATYPES(ID, AGE)
+
+VALUES(10000, 34);
+
+INSERT INTO STRONGIN.DATATYPES(ID, AGE)
+
+VALUES(10001, 32);
+
+-- Implicit data type conversion
+
+INSERT INTO STRONGIN.DATATYPES(ID, AGE)
+
+VALUES('10002', 32);
+
+-- Implicit data type conversion
+
+INSERT INTO STRONGIN.DATATYPES(ID, AGE)
+
+VALUES('10003', '35');
+
+-- Implicit data type conversion
+
+INSERT INTO STRONGIN.DATATYPES(ID, AGE)
+
+VALUES('19700101', '35');
+
+-- Implicit data type conversion IS NOT WORKING!
+
+INSERT INTO STRONGIN.DATATYPES(ID, AGE)
+
+VALUES('1970-01-01', '35');
+
+
+-- Let's take a look on the data
+
+SELECT *
+
+FROM STRONGIN.DATATYPES;
